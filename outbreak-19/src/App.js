87b8ce580a790305d283e01dc49a55813ae40694
SagarGi <sagargurung1001@gmail.com>
2020-04-04 10:33:37 +0545

    first commit

---
+++ outbreak-19/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component
+{
+  render()
+  {
+    return(
+      <div className = "main">
+          <div className = "container main-content">
+            <div className = "card wrapper">
+
+              {/* to be continued */}
+              
+            </div>
+          </div>
+      </div>
+    )
+  }
+}
+export default App;
