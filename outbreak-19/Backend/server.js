f8269f08bd6a1853c97a2ae32424e430fecf6a71
SagarGi <sagargurung1001@gmail.com>
2020-04-05 09:01:41 +0545

    server

---
+++ outbreak-19/Backend/server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const request = require('request')
+const bodyParser = require('body-parser')
+
+
+const app = express()
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+const port = 3001;
+
+app.get('/covid-19/country', (req,res,next) =>{
+    request.get('https://corona.lmao.ninja/all', (error,response,body) =>{
+        res.send(body)
+    })
+})
+
+app.post('/covid-19',(req,res,next) =>{
+
+    const country = req.body.country1
+    console.log(country)
+    request.get(`https://corona.lmao.ninja/countries/${country}`, (error,response,body) =>{
+        res.send(body)
+    })
+
+})
+
+
+app.listen(port, ()=>{
+    console.log("Server is listening...........3001")
+})
+
