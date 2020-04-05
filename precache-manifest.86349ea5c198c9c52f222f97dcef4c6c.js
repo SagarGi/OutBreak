5279fcba333252993e225c28c4572a6ac3723bfe
SagarGi <sagargurung1001@gmail.com>
2020-04-05 18:29:36 +0545

    Updates

---
+++ precache-manifest.86349ea5c198c9c52f222f97dcef4c6c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1ea400197564161b02b60a44dacbb08",
+    "url": "/outbreak/index.html"
+  },
+  {
+    "revision": "3c092eb024509e214438",
+    "url": "/outbreak/static/css/main.fc02e1bb.chunk.css"
+  },
+  {
+    "revision": "37abf3ff0043d29b758a",
+    "url": "/outbreak/static/js/2.7aca7a1e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/outbreak/static/js/2.7aca7a1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c092eb024509e214438",
+    "url": "/outbreak/static/js/main.54dc47d6.chunk.js"
+  },
+  {
+    "revision": "04aa4fb3d86ca248afae",
+    "url": "/outbreak/static/js/runtime-main.653bd7c9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/outbreak/static/media/logo.5d5d9eef.svg"
+  }
+]);
